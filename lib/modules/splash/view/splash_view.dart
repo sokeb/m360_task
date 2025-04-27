b877512fbada67e99d858c15179efbc1138becbb
mksakib126 <mksakib126@gmail.com>
2025-04-28 00:47:36 +0600

    chore: upate splash screen

--- lib/modules/splash/view/splash_view.dart
+++ lib/modules/splash/view/splash_view.dart
@@ -1,41 +1,67 @@
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Future<void>.delayed(
+      const Duration(seconds: 2),
+      () {},
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            child: Center(
-              child: Text(
-                'Splash Screen',
-                style: Theme.of(context).textTheme.headlineMedium,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Align(
+              child: Center(
+                child: AspectRatio(
+                    aspectRatio: 214 / 100,
+                    child: Image.asset(
+                      'assets/icons/logo.png',
+                    )),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 30),
-              child: RichText(text: TextSpan(
-                text: 'Power By ',
-                style: Theme.of(context).textTheme.headlineMedium,
-                children: [
-                  TextSpan(
-                    text: 'M360 ICT',
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: RichText(
+                  text: const TextSpan(
+                    text: 'Powered by ', // Fixed typo in "Powered by"
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).colorScheme.primary,
+                      fontFamily: 'Inter',
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFFADADAD),
                     ),
+                    children: [
+                      TextSpan(
+                        text: 'M360 ICT',
+                        style: TextStyle(
+                          fontFamily: 'Inter',
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF47BA80),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
